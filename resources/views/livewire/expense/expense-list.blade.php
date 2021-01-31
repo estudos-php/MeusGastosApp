7c79145efdfcfe04a28934f6ee8a96d5cbf0fca0
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2021-01-31 16:08:10 -0300

    Update expense-list.blade.php

--- resources/views/livewire/expense/expense-list.blade.php
+++ resources/views/livewire/expense/expense-list.blade.php
@@ -1,40 +1,46 @@
-<div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-            <x-slot name="header">
-                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                    Alterar Registro
-                </h2>
-            </x-slot>
-            @if(session()->has('message'))
-                <h3>{{ session('message') }}</h3>
-            @endif
-            <table>
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Descrição</th>
-                    <th>Valor</th>
-                    <th>Data de Registro</th>
-                    <th>Ações</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($expenses as $expense)
-                    <tr>
-                        <td>{{ $expense->id }}</td>
-                        <td>{{ $expense->description }}</td>
-                        <td>{{ $expense->amount }}</td>
-                        <td>{{ $expense->created_at->format('d/m/Y H:i:s') }}</td>
-                        <td>
-                            <a href="{{ route('expenses.edit', $expense->id) }}">Editar</a>
-                            <a href="#" wire:click.prevent="removeExpense({{ $expense->id }})">Remover</a>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-            {{ $expenses->links() }}
-        </div>
+<div class="max-w-7xl mx-auto py-15 px-4">
+
+    <x-slot name="header">
+        Meus Registros
+    </x-slot>
+
+    <div class="w-full mx-auto text-right mb-4">
+        <a href="{{route('expenses.create')}}"
+           class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded">Criar
+            Registro</a>
+    </div>
+
+    @include('includes.message')
+
+    <table class="table-auto w-full mx-auto">
+        <thead>
+        <tr class="text-left">
+            <th class="px-4 py-2">#</th>
+            <th class="px-4 py-2">Descrição</th>
+            <th class="px-4 py-2">Valor</th>
+            <th class="px-4 py-2">Data Registro</th>
+            <th class="px-4 py-2">Ações</th>
+        </tr>
+        </thead>
+
+        <tbody>
+        @foreach($expenses as $exp)
+            <tr>
+                <td class="px-4 py-2 border">{{$exp->id}}</td>
+                <td class="px-4 py-2 border">{{$exp->description}}</td>
+                <td class="px-4 py-2 border">{{$exp->amount}}</td>
+                <td class="px-4 py-2 border">{{$exp->created_at->format('d/m/Y H:i:s')}}</td>
+                <td class="px-4 py-4 border">
+                    <a href="{{route('expenses.edit', $exp->id)}}"
+                       class="px-4 py-2 border rounded bg-blue-500 text-white">Editar</a>
+                    <a href="#" wire:click.prevent="remove({{$exp->id}})"
+                       class="px-4 py-2 border rounded bg-red-500 text-white">Remover</a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    <div class="w-full mx-auto mt-10">
+        {{$expenses->links()}}
     </div>
 </div>
