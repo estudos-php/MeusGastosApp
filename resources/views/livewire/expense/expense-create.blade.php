51c3e181b095cf2b211af77596b4cf3b2dd0757a
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2021-01-31 16:08:00 -0300

    Update expense-create.blade.php

--- resources/views/livewire/expense/expense-create.blade.php
+++ resources/views/livewire/expense/expense-create.blade.php
@@ -1,44 +1,62 @@
-<div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-            <x-slot name="header">
-                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                    Criar um Registro
-                </h2>
-            </x-slot>
-            @if(session()->has('message'))
-                <h3>{{ session('message') }}</h3>
-            @endif
-            <form wire:submit.prevent="createExpense">
-                <div>
-                    <label for="description">Descrição do Registro</label>
-                    <input type="text" name="description" wire:model="description"/>
-                    @error('description')
-                    <h5>{{ $message }}</h5>
-                    @enderror
-                </div>
-                <div>
-                    <label for="amount">Valor do Registro</label>
-                    <input type="text" name="amount" wire:model="amount"/>
-                    @error('amount')
-                    <h5>{{ $message }}</h5>
-                    @enderror
-                </div>
-                <div>
-                    <label for="type">Tipo do Registro</label>
-                    <select name="type" wire:model="type">
-                        <option value="">Selecione o tipo de Registro</option>
-                        <option value="1">Entrada</option>
-                        <option value="2">Saída</option>
-                    </select>
-                    @error('type')
-                    <h5>{{ $message }}</h5>
-                    @enderror
-                </div>
-                <div>
-                    <button type="submit">Criar Registro</button>
-                </div>
-            </form>
+<div class="max-w-7xl mx-auto py-15 px-4">
+
+    <x-slot name="header">
+        Criar Registro
+    </x-slot>
+
+    @include('includes.message')
+
+    <form action="" wire:submit.prevent="createExpense" class="w-full max-w-7xl mx-auto">
+        <div class="flex flex-wrap -mx-3 mb-6">
+
+            <div class="w-full px-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Descrição
+                    Registro</label>
+                <input type="text" name="description" wire:model="description"
+                       class="block appearance-none w-full bg-gray-200 border @error('description') border-red-500 @else border-gray-200 @enderror  text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+
+                @error('description')
+                <h5 class="text-red-500 text-xs italic">{{$message}}</h5>
+                @enderror
+            </div>
+
+
+            <div class="w-full px-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Valor do
+                    Registro</label>
+                <input type="text" name="amount" wire:model="amount"
+                       class="block appearance-none w-full bg-gray-200 border @error('amount') border-red-500 @else border-gray-200 @enderror  text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+
+                @error('amount')
+                <h5 class="text-red-500 text-xs italic">{{$message}}</h5>
+                @enderror
+
+            </div>
+
+
+            <div class="w-full px-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Tipo do
+                    Registro</label>
+                <select name="type" id="" wire:model="type"
+                        class="block appearance-none w-full bg-gray-200 border @error('type') border-red-500 @else border-gray-200 @enderror  text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                    <option value="">Selecione o tipo do registro: Entrada ou Saída...</option>
+                    <option value="1">Entrada</option>
+                    <option value="2">Saída</option>
+                </select>
+
+                @error('type')
+                <h5 class="text-red-500 text-xs italic">{{$message}}</h5>
+                @enderror
+            </div>
+
         </div>
-    </div>
+        <div class="w-full py-4 px-3 mb-6 md:mb-0">
+
+            <button type="submit"
+                    class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded">
+                Criar Registro
+            </button>
+        </div>
+
+    </form>
 </div>
